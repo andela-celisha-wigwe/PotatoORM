4ecb37c9a2d0f6744bbdf06d697d74d10f57283e
andela-celisha-wigwe <chijioke.elisha-wigwe@andela.com>
2016-03-04 18:54:41 +0100

    [Features] Add a BaseModel class plus coverage configurations
    The BaseModel class is responsible to handling basic database CRUD operations.
    Two methods are included - getAll() and connect().
    A new test class is the BaseModelTest class to handle tests for the BaseModel class.
    Also included some coverage, and testing configurations with composer and phunit.

---
+++ tests/BaseModelTest.php
@@ -0,0 +1,29 @@
+<?php
+
+class BaseModelTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGetAllFunctionWorks()
+    {
+    }
+
+    public function testFindFunctionWorks()
+    {
+
+    }
+
+    public function testSavefunctionWorks()
+    {
+
+    }
+
+    public function testUpdateFunctionworks()
+    {
+
+    }
+
+    public function testDestroyFunctionWorks()
+    {
+
+    }
+
+}
