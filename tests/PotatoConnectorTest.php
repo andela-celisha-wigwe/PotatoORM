478873bbf6f823ba06b103e00683eec43d42e74a
andela-celisha-wigwe <chijioke.elisha-wigwe@andela.com>
2016-03-22 18:58:04 +0100

    [Fix] Sqlite connection issues

--- tests/PotatoConnectorTest.php
+++ tests/PotatoConnectorTest.php
@@ -10,16 +10,12 @@ class PotatoConnectorTest extends PHPUnit_Framework_TestCase
     private $connector;
     private $mockConnection;
     private $configFile;
-    private $sqliteFile;
-    private $testClass;
     private $expectedconfig;
     private $adaptar;
     private $host;
     private $dbname;
     private $username;
     private $password;
-    private $dsn;
-    private $db;
 
     public function setUp()
     {
@@ -29,22 +25,18 @@ class PotatoConnectorTest extends PHPUnit_Framework_TestCase
                 'password' => '',
                 'dbname'   => 'mydb',
                 'adaptar'  => 'sqlite',
+                'sqlite_file' => 'sample.db'
             ];
         $this->adaptar = $this->expectedconfig['adaptar'];
         $this->host = $this->expectedconfig['host'];
         $this->dbname = $this->expectedconfig['dbname'];
         $this->username = $this->expectedconfig['username'];
         $this->password = $this->expectedconfig['password'];
-        $this->dsn = "$this->adaptar:host=$this->host;dbname=$this->dbname";
 
         $this->root = vfsStream::setup('home');
         $this->configFile = vfsStream::url('home/config.ini');
-        $this->sqliteFile = vfsStream::url('home/db.sqlite');
-        $this->testClass = vfsStream::url('home/Book.php');
-        $this->mockConnection = m::mock('PDO', [$this->dsn, $this->username, $this->password]);
 
         $this->connector = new PotatoConnector($this->expectedconfig);
-        // $this->mockConnection = m::mock('PDO');
     }
 
     public function testGetAdaptar()
@@ -61,7 +53,7 @@ class PotatoConnectorTest extends PHPUnit_Framework_TestCase
 
     public function testSqliteConnect()
     {
-        $result = $this->connector->sqliteConnect('sqlite', __DIR__.'/../db.sqlite');
+        $result = $this->connector->sqliteConnect('sqlite', 'sample.db');
         $this->assertInstanceOf('PDO', $result);
     }
 
@@ -73,10 +65,8 @@ class PotatoConnectorTest extends PHPUnit_Framework_TestCase
 
     public function testConnectDriverForMySql()
     {
-        // Create the Database using mysqli.
         $conn = mysqli_connect('127.0.0.1', 'root', '');
         mysqli_query($conn, 'CREATE DATABASE IF NOT EXISTS elchroy');
-        // $mockPDOSqlite = m::mock('PDO', ["mysql:host=127.0.0.1;dbname=dbname", "root", ""]);
         $result = $this->connector->connectDriver('mysql', '127.0.0.1', 'elchroy', 'root', '');
         $this->assertInstanceOf('PDO', $result);
         mysqli_query($conn, 'DROP DATABASE elchroy'); //Destroy the database;
@@ -90,44 +80,6 @@ class PotatoConnectorTest extends PHPUnit_Framework_TestCase
     public function testConnectDriverforException()
     {
         $result = $this->connector->connectDriver('wrongAdaptar', 'host', 'dbname', 'username', 'password');
-    }
-
-    public function notestSqliteConnectLocatesFile()
-    {
-        //Try using SQLite to write to the mock file. Not easy though.
-        $result = $this->connector->getSqliteFile();
-        $this->assertEquals('/Users/user/Code/learn/checkpoint2/potatoorm/src/../db.sqlite', $result);
-    }
-
-    public function notestDB()
-    {
-        $dbFileMock = fopen($this->sqliteFile, 'w');
-        $dbContents = file_get_contents('testDB.sqlite');
-        fwrite($dbFileMock, $dbContents);
-        fclose($dbFileMock);
-
-        $classFile = fopen($this->testClass, 'w');
-        $classContents = "<?php class Dog extends Elchroy\PotatoORM\PotatoModel { } ?>";
-        fwrite($classFile, $classContents);
-        fclose($classFile);
-
-        $db = fopen('testDB.sqlite', 'r');
-        var_dump($db);
-
-        $sql = 'SHOW TABLES';
-        // $nc = new PDO("sqlite:".$db);
-        $nc = new PDO('sqlite:testDB.sqlite');
-        $rs = $nc->exec($sql);
-        var_dump($rs);
-        $fstmt = m::mock('PDOStatement');
-        $fconnection = m::mock('PDO', ["sqlite:$db"]);
-        $fconnection->shouldReceive('prepare')->with($sql)->andReturn($fstmt);
-        $fstmt = $fconnection->prepare($sql);
-        $fstmt->shouldReceive('execute');
-        $fstmt->shouldReceive('fetchObject')->andReturn(new stdClass());
-        $fstmt->execute();
-        $result = $fstmt->fetchObject();
-        // var_dump($result);
     }
 
     public function testGetUsername()
@@ -158,6 +110,7 @@ class PotatoConnectorTest extends PHPUnit_Framework_TestCase
                     'password = ',
                     'dbname = mydb',
                     'adaptar = sqlite',
+                    'sqlite_file = sample.db'
             ];
         foreach ($configData as $cfg) {
             fwrite($file, $cfg."\n");
@@ -165,11 +118,6 @@ class PotatoConnectorTest extends PHPUnit_Framework_TestCase
         fclose($file);
         $result = $this->connector->getConfigurations($this->configFile);
         $this->assertEquals($this->expectedconfig, $result);
-    }
-
-    public function testGetConfigurationsIfNotGivenFilePath()
-    {
-        // $connector = new PotatoConnector();
     }
 
     public function testGetConFilePath()
